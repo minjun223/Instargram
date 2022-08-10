315adbcaee6adf20a1aa46d6aa343376968156f5
김민준 <80810346+minjun223@users.noreply.github.com>
2022-08-10 12:17:08 +0900

    instargram

---
+++ app/src/main/java/com/example/instargramactivity/post.kt
@@ -0,0 +1,9 @@
+package com.example.instargramactivity
+
+import java.io.Serializable
+
+class post (
+    val owner : String? = null,
+    var content : String? = null,
+    var image : String? = null
+):Serializable
